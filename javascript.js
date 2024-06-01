0e205b9ca7fb9c7a5b33c3f014306691d7defec6
Sempanha123 <sempanha02@gmail.com>
2024-06-01 13:29:03 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,40 @@
+// $(window).scroll( () => {
+//     var windowTop = $(window).scrollTop();
+//     windowTop > 200 ? $('header').addClass('sticky-top') : $('header').removeClass('sticky-top');
+//     windowTop > 200 ? $('.header-top').addClass('d-none') : $('.header-top').removeClass('d-none');
+//     alert(0);
+// });
+// A $( document ).ready() block.
+$(document).ready(function () {
+  const swiper1 = new Swiper(".pic4-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar"
+    }
+  });
+  const swiper2 = new Swiper(".pic5-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    loop: true,
+
+    autoplay: {
+      delay: 2000
+    },
+
+    breakpoints: {
+
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      
+    }
+  });
+});
